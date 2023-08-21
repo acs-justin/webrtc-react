18990d4efb26e00177d19d8cf39e27f64b2f29e7
acs-justin <justin@allencontrolsystems.com>
2023-08-21 17:36:57 -0400

    changed logging again again

--- peerjs/src/index.ts
+++ peerjs/src/index.ts
@@ -3,19 +3,26 @@ import { PeerServer } from "peer";
 const server = PeerServer({ port: 9001, path: "/" });
 
 server.on("connection", (client) => {
-    console.log("CLIENT CONNECTED:")
+    console.log("****************** CLIENT CONNECTED: ******************")
     console.log(client)
 })
 
 server.on("disconnect", (client) => {
-    console.log("CLIENT DISCONNECTED:")
+    console.log("****************** CLIENT DISCONNECTED: ******************")
     console.log(client)
 })
 
 server.on("message", (client, message) => {
-    console.log(`Message: ${message.toString()} Client: ${client.toString()}`)
+    console.log("****************** Message and Client ******************")
+    console.log(message)
+    console.log("****************** from client ******************")
+    console.log(client)
+    console.log("****************** END CLIENT ******************")
+
 })
 
 server.on('error', (error) => {
-    console.log(`ERROR ${error}`)
+    console.log("****************** ERROR: ******************")
+    console.log(error)
+    console.log("****************** END ERROR ******************")
 })

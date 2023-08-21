9f74525d74614ce19fccf37bfeff702f07585ebd
acs-justin <justin@allencontrolsystems.com>
2023-08-21 12:07:32 -0400

    added more logs to peerjs

--- peerjs/src/index.ts
+++ peerjs/src/index.ts
@@ -3,9 +3,17 @@ import { PeerServer } from "peer";
 const server = PeerServer({ port: 9001, path: "/" });
 
 server.on("connection", (client) => {
-    console.log(client)
+    console.log(`CLIENT CONNNECTED ${client}`)
 })
 
 server.on("disconnect", (client) => {
-    console.log(client)
+    console.log(`CLIENT DISCONNECTED ${client}`)
+})
+
+server.on("message", (client, message) => {
+    console.log(`Message: ${message} Client: ${client}`)
+})
+
+server.on('error', (error) => {
+    console.log(`ERROR ${error}`)
 })
